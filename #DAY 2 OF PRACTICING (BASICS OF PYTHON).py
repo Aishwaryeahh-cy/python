b3b828f01e6d31a00d90e9beb4806d2e92215c97
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2025-10-04 14:59:17 +0530

    Add day 2 practice file

---
+++ #DAY 2 OF PRACTICING (BASICS OF PYTHON).py
@@ -0,0 +1,47 @@
+#DAY 2 OF PRACTICING (BASICS OF PYTHON)
+#type conversion
+a=3
+b=4.5
+sum=a+b
+print(sum)
+a=int("6")
+b=66
+print(type(a))
+print(a+b)
+a=int("90")
+b=90.8
+print(type(a))
+print(a+b)
+a=99
+a=str(a)
+print(type(a))
+name=input("YOUR GOOD NAME:")
+print("Hey there!!",name)
+val=int(input("your age:"))
+print("hey ur age:",val)
+value=float(input("Your percentage:"))
+print ("Thats ur percentage:",value)
+name=input("Please enter ur name:")
+print("your name:",name)
+age=int(input ("Enter your age:"))
+print("your present age",age)
+per=float(input("Enter ur percentage:"))
+print("your Percentage:",per)
+a=int(input("Enter value 1:"))
+b=int(input("Enter value 2:"))
+sum=a+b
+print("your sum is:",sum)
+a=int(input("Enter ur input size value:"))
+a=a*a
+print("The answer is:",a)
+a=int(input("Enter ur input size value:"))
+print("The answer is:",a**2)
+a=int(input("Enter 1st val:"))
+b=int(input("Enter 2nd val:"))
+print("Average :",(a+b)/2)
+print("The answer is:",a**2)
+a=int(input("Enter 1st val:"))
+b=int(input("Enter 2nd val:"))
+print(a>=b)
+print(a<b)
+
