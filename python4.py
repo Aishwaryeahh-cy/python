305e6a20dbb63b1ce7ed64c20f62988f8f82c554
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2025-10-05 19:43:23 +0530

    pthon4.py

---
+++ python4.py
@@ -0,0 +1,83 @@
+#DAY 4 OF PRACTICING (BASICS OF PYTHON)
+# a=20
+# if (a>=18):
+#     print("Yes,ur old enough")
+#     print("good")
+age=56
+# if(True):
+#     print("Your a senior citizen")
+#     print("Well")
+# lights="red"
+# if (lights=="red"):
+#     print("wait")
+# elif(lights=="yellow"):
+#     print("wait")
+# elif(lights=="green"):
+#     print("proceed")    
+# lights=input("Enter ur choice red or green or yellow:")
+# print(lights)
+# if (lights=="red"):
+#     print("wait")
+# elif(lights=="yellow"):
+#     print("wait")
+# elif(lights=="green"):
+#     print("proceed")        
+# lights=input("Enter ur choice red or green or yellow:")
+# print(lights)
+# if (lights=="red"):
+#     print("wait")
+# elif(lights=="yellow"):
+#     print("wait")
+# elif(lights=="green"):
+#     print("proceed")  
+# else:
+#     print("Light is off:")
+# age=int(input("Enter your respective age:"))
+# print(age)
+# if(age<=5):
+#     print("young")#indentation means leaving proper spacing
+# else:
+#     print("incorrect")
+# marks=int(input("Enter your marks:"))
+# print(marks)
+# if (marks>=90):
+#     print(" A")
+# elif (marks>=80 and marks<=90):
+#     print("B")
+# elif (marks>=70 and marks<=80 ):
+#     print("C")
+# elif (marks <70):
+#     print("D")
+# else:
+#     print("Absent")
+# amount=int(input("Enter ur amount:"))
+# print(amount)
+# if(amount<=100):
+#     if(amount<=20):
+#         print("Kitkat")
+#     else:
+#         print("No money")
+# else:
+#     print("Bye")    
+# num=int(input("Enter a number:"))
+# print(num)
+# if(num%2==0):
+#     print("Even")   
+# else:
+#     print("Odd")         
+# a=int(input("Enter a value for a:"))
+# b=int(input("Enter a value for b:"))
+# c=int(input("Enter a value for c:"))
+# print(a,b,c)
+# if (a>b and a>c):
+#     print("A is greatest")
+# elif (b>a and b>c):
+#     print("B is greatest") 
+# elif (c>a and c>b):
+#     print("C is greatest")        
+x=int(input("Enter a number:"))
+print(x)
+if(x%7==0):
+    print("Multiple of 7")
+else:
+    print("Not")    
