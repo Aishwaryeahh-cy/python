007fab56c3143b35c956a768d433d1d125d26313
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2025-10-05 11:31:25 +0530

    Update python3.py

--- python3.py
+++ python3.py
@@ -1,60 +1,60 @@
 #DAY 3 OF PRACTICING (BASICS OF PYTHON)
-# str1 = "This is a string"
-# str2 ='This is a string'
-# str3 ="""This is a string"""
-# str4 ="This is my new project\n About python"
-# print(str4)
-# print("Heyyyyy\t there")
-# str5="aish"
-# str6="warya"
-# final=str5+str6
-# print(final)
-# str7="aish"
-# str8="u"
-# print(str7+str8)
-# str9="aissssssssssshhhhhhhhhhhhhhhhhhwaaaaaaarrrrrryyyyyyyyyyyyyyyyssssssssssss"
-# str=len(str9)
-# print(str)
-# name="Mikasa"
-# print(len(name))
-# val="eren yeager"
-# val2="  "
-# val1=len(val+val2)
-# print(val1)
-# strr="apna"
-# ch=strr[0]
-# print(ch)
-# nam="myself"
-# har=nam[3]
-# print(har)
-# name="saske"
-# print(name[4])
-# da="aaaa   "
-# print(da[5])
-#slicing
-# new="newswet topic"
-# print(new[1:4])
-# new="newswet topic"
-# print(new[0:4])
-# new="newswet topic"
-# print(new[:13])
-# old="a           pppp pppp"
-# print(old[4:len(old)])
-# new="newswet topic"
-# print(new[1:])
-# str="apple"
-# print(str[-3:-1])
-# str2="apple"
-# print(str2[-6:-1])
-# str="I am a coder"
-# print(str.endswith("er"))
-# print(str.capitalize())
-# print(str)
-# print(str.replace("o","a"))
-# print(str.find("Q"))
-# print(str.count("a"))
-# name=input("Enter your name")
-# print("The length is:",len(name))
-# occur="aaaaaaaaaaaassssssssshhhh$$$$$$$$$$$$$$$$$$$"
-# ns=occur
-# print(ns.count("$"))
+str1 = "This is a string"
+str2 ='This is a string'
+str3 ="""This is a string"""
+str4 ="This is my new project\n About python"
+print(str4)
+print("Heyyyyy\t there")
+str5="aish"
+str6="warya"
+final=str5+str6
+print(final)
+str7="aish"
+str8="u"
+print(str7+str8)
+str9="aissssssssssshhhhhhhhhhhhhhhhhhwaaaaaaarrrrrryyyyyyyyyyyyyyyyssssssssssss"
+str=len(str9)
+print(str)
+name="Mikasa"
+print(len(name))
+val="eren yeager"
+val2="  "
+val1=len(val+val2)
+print(val1)
+strr="apna"
+ch=strr[0]
+print(ch)
+nam="myself"
+har=nam[3]
+print(har)
+name="saske"
+print(name[4])
+da="aaaa   "
+print(da[5])
+slicing
+new="newswet topic"
+print(new[1:4])
+new="newswet topic"
+print(new[0:4])
+new="newswet topic"
+print(new[:13])
+old="a           pppp pppp"
+print(old[4:len(old)])
+new="newswet topic"
+print(new[1:])
+str="apple"
+print(str[-3:-1])
+str2="apple"
+print(str2[-6:-1])
+str="I am a coder"
+print(str.endswith("er"))
+print(str.capitalize())
+print(str)
+print(str.replace("o","a"))
+print(str.find("Q"))
+print(str.count("a"))
+name=input("Enter your name")
+print("The length is:",len(name))
+occur="aaaaaaaaaaaassssssssshhhh$$$$$$$$$$$$$$$$$$$"
+ns=occur
+print(ns.count("$"))
