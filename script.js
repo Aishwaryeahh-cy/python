dde2e184e1c3a2dc0e8b6e747f8aefb6c2c5b141
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2025-10-28 22:35:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let a=20;
+    console.log(a);
+    a="hello";
+    console.log(a);
+     a="Hey!";
+    console.log(a);
+
+// Single line Comment
+// let a=20;
+//     console.log(a);
+//     a="hello";
+//     console.log(a);
+//      a="Hey!";
+//     console.log(a);    
+
+
+/* */ Multi Line Comment
+/*
+let a=20;
+    console.log(a);
+    a="hello";
+    console.log(a);
+     a="Hey!";
+    console.log(a); 
+*/
+
+
+
+    
+
