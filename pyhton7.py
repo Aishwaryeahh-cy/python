0912b9cdd93d7045341559da0a647fb3a15ac8ea
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2025-11-24 22:19:55 +0530

    update

---
+++ pyhton7.py
@@ -0,0 +1,21 @@
+#python 7 day
+# f=open("python1.py","r")
+# data = f.readline()
+# print(data)
+# print(type(data))
+# f.close()
+# f=open("python1.py","a")
+# f.write("\nhey new textttttttttttttttttt  herjhuhuhuihuiuihkieeeeeeeeeeeeeeeeee")
+# f.close()
+# f = open("sample.txt","a")
+# f.close()
+# f = open("practice.txt","w")
+# f.write("Hi everyone, /n we are learning File I/O, /n using java /n I like programming in Java")
+# f.close()
+import os
+print("Current folder:", os.getcwd())
+
+with open("practice.txt","w") as f:
+    f.write("Hi everyone, \n we are learning File I/O, \n using java \n I like programming in Java")
+
+
--- python me.py
+++ python me.py
@@ -1,25 +1,22 @@
-# def show(n):
-# if (n==0):
-# return
-# print(n)
-# show(n-1)
-# def show(n):
-#     if (n==0):
-#         return
-#     print(n)
-#     show (n-1)
-# show(5)
-# def show(a):
-#     if (a==70):
-#         return
-#     print(a)
-#     show(a-1)
-# show(90)  
-# def fact(n):
-#     if (n==0 or n==1):
-#         return 1
-#     return fact(n-1)*n
-# print(fact(4))
+
+show(n-1)
+def show(n):
+    if (n==0):
+        return
+    print(n)
+    show (n-1)
+show(5)
+def show(a):
+    if (a==70):
+        return
+    print(a)
+    show(a-1)
+show(90)  
+def fact(n):
+    if (n==0 or n==1):
+        return 1
+    return fact(n-1)*n
+print(fact(4))
 def sum(n):
     if(n==0):
         return 0
