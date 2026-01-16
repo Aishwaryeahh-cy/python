3a0bc2fb5781908faa0cad2a22129d7f53c1ce9d
Aishwaryeahh-cy <vaishwarya990@gmail.com>
2026-01-16 19:41:31 +0530

    pytho

--- pythonn.py
+++ pythonn.py
@@ -1,22 +1,45 @@
-print("I am learning python")
-name="aish"
-num=5
-city="Bengaluru"
-print(name,num,city)
-10
-
-x=100
-print(type(100))
-a=10
-b=2.5
-print(a+b)
-int
-age=10
-height="5'4"
-is_coder=101
-print(type(age),age)
-print(type(height),height)
-print(type(is_coder),is_coder)
-
-age=int(input("Enter your age:"))
-print("Your age after 5 years will be:",age+5)
+# print(10 % 4)
+# print(5 > 3 and 2 < 1)
+# a= int(input ("Enter a num for a:"))
+# b= int(input ("Enter a num for b:"))
+# sum = a+b 
+# print(sum)
+# product = a*b
+# print(product)
+# big = a>b and b>a
+# print(big)
+# a= int(input("Enter a number a:"))
+# if a>0:
+#     print("Positive")
+# elif a<0:
+#     print("Negative")
+# elif a==0:
+#     print("Equal")
+# else:
+#     print("Enter a correct input")
+# age=int(input("Enter your age:"))
+# if age<13:
+#     print("Child")
+# elif age(13,14,15,16,17,18,19):
+#     print("Teen")
+# elif age>=20:
+#     print("Adult")
+# else:
+#     print("Enter your age properly:")
+# for i in range(3):
+#     print(i)
+i = 1
+# while i<4:
+#     print(i)
+#     i+=1
+# while(i<11):
+#     print(i)
+#     i+=1
+# for i in range(1,10):
+#     print(i)
+        
+a=int(input("Enter a num:"))
+b=1
+while (b<=10):
+    print(a*b)
+    b+=1
